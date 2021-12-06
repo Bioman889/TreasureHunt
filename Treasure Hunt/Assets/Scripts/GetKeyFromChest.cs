fe4b3ea400c7939079ac774864da374bb1f6b5d9
Bioman889 <56267855+Bioman889@users.noreply.github.com>
2021-12-06 13:22:28 -0500

    Final push on my end

---
+++ Treasure Hunt/Assets/Scripts/GetKeyFromChest.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetKeyFromChest : MonoBehaviour
+{
+    [SerializeField] KeyScriptObj KeyInt;
+    public Text pressE;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+    
+    }
+}
---
+++ Treasure Hunt/Assets/Scripts/KeyRemember/KeyScriptObj.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Scriptable Int", menuName = "Key Counter")]
+public class KeyScriptObj : ScriptableObject
+{
+    public int value;
+}
